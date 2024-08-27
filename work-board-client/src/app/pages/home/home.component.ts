36bccaad94e8ea12980ca605a6d15be691c154aa
Vu Quoc Tuan <tuan-vq@fujinet.net>
2024-08-27 17:42:49 +0700

    commit src

--- work-board-client/src/app/pages/home/home.component.ts
+++ work-board-client/src/app/pages/home/home.component.ts
@@ -1,7 +1,6 @@
-import { Dialog, DIALOG_DATA } from '@angular/cdk/dialog';
+import { Dialog } from '@angular/cdk/dialog';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-import { NgIf } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { DialogAddTaskComponent } from '../dialog-add-task/dialog-add-task.component';
 export interface DialogData {
   animal: 'panda' | 'unicorn' | 'lion';
@@ -25,11 +24,13 @@ export class HomeComponent {
     });
   }
 
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  newTask = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  inProgress = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
-  test = ['Get up1', 'Brush teeth2', 'Take a shower3', 'Check e-mail4', 'Walk dog5'];
+  pending = ['Get up1', 'Brush teeth2', 'Take a shower3', 'Check e-mail4', 'Walk dog5'];
+
+  completed = ['Get up2', 'Brush teeth3', 'Take a shower4', 'Check e-mail5', 'Walk dog6'];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {

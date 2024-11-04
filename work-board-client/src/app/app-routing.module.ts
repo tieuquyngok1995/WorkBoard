cb284d77f22f0315e85caa66072196ae599fe85c
Vu Quoc Tuan <tuan-vq@fujinet.net>
2024-11-04 10:07:18 +0700

    Update app-routing.module.ts

--- work-board-client/src/app/app-routing.module.ts
+++ work-board-client/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { ErrorComponent } from './pages/error/error.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', component: HomeComponent, canActivate: [AuthGuardService] },
+  { path: '', component: HomeComponent, canActivate: [AuthGuardService], pathMatch: 'full' },
   { path: '404', component: ErrorComponent },
   { path: '**', redirectTo: '/404' }
 ];
